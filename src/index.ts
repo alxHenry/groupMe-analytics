faf0f41a8eaf8bb1283c383e6c977cf44734908f
Alex Henry <adhe223@g.uky.edu>
2018-10-31 21:49:11 -0700

    Update index as I'm wokring through functionality

--- src/index.ts
+++ src/index.ts
@@ -2,39 +2,47 @@ import { getMessagesAverageSentiment } from './apiConsumers/azureCognitiveServic
 import { fetchAllGroupMessages } from './apiConsumers/groupMeFetcher';
 import { getGroupMessages, saveGroupMessages } from './dbHelper';
 import { getMessagesByUserMap, getUserIdToNameMap } from './messages/utils';
+import db, { shutdownDB } from './database';
 
-const groupId = '45622290';
+const groupId = '13207297'; // '45622290' '9817284';
 
 const start = async () => {
   let messages = await getGroupMessages(groupId);
 
   if (!messages.length) {
     messages = await fetchAllGroupMessages(groupId);
-    saveGroupMessages(messages);
+    console.log(`Completed fetching ${messages.length} messages.`);
+
+    await saveGroupMessages(messages);
+    console.log('Successfully wrote to the db!');
   }
 
-  const messagesByUserMap = getMessagesByUserMap(messages);
-  const userIdToNameMap = getUserIdToNameMap(messagesByUserMap);
-  const sentimentsByUserMap: { [userId: string]: number } = {};
+  // const messagesByUserMap = getMessagesByUserMap(messages);
+  // const userIdToNameMap = getUserIdToNameMap(messagesByUserMap);
+  // const sentimentsByUserMap: { [userId: string]: number } = {};
 
-  const acsPromises: Promise<number>[] = [];
-  Object.keys(messagesByUserMap).forEach(userId => {
-    acsPromises.push(getMessagesAverageSentiment(messagesByUserMap[userId]));
-  });
+  // const acsPromises: Promise<number>[] = [];
+  // Object.keys(messagesByUserMap).forEach(userId => {
+  //   acsPromises.push(getMessagesAverageSentiment(messagesByUserMap[userId]));
+  // });
 
-  const messagesByUserKeys = Object.keys(messagesByUserMap);
-  const sentimentValues = await Promise.all(acsPromises);
+  // const messagesByUserKeys = Object.keys(messagesByUserMap);
+  // const sentimentValues = await Promise.all(acsPromises);
 
-  sentimentValues.forEach((value, index) => {
-    const correspondingUserId = messagesByUserKeys[index];
-    sentimentsByUserMap[correspondingUserId] = value;
+  // sentimentValues.forEach((value, index) => {
+  //   const correspondingUserId = messagesByUserKeys[index];
+  //   sentimentsByUserMap[correspondingUserId] = value;
 
-    console.log(
-      `Average sentiment for user ${userIdToNameMap[correspondingUserId]} is ${
-        sentimentsByUserMap[correspondingUserId]
-      }`
-    );
-  });
+  //   console.log(
+  //     `Average sentiment for user ${userIdToNameMap[correspondingUserId]} is ${
+  //       sentimentsByUserMap[correspondingUserId]
+  //     }`
+  //   );
+  // });
 };
 
 start();
+
+process.on('SIGTERM', () => {
+  shutdownDB();
+});
